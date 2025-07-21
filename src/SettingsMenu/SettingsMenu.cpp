1a937597d35eb1bbc84b584121469f40e40b4b2d
Roi Romem <130078520+RoiRomem@users.noreply.github.com>
2025-07-21 22:34:21 +0300

    v0.3b, fixed bugs, optimized code, and added a settings tab

---
+++ src/SettingsMenu/SettingsMenu.cpp
@@ -0,0 +1,5 @@
+//
+// Created by owner on 7/21/2025.
+//
+
+#include "SettingsMenu.h"
---
+++ src/SettingsMenu/SettingsMenu.h
@@ -0,0 +1,16 @@
+//
+// Created by owner on 7/21/2025.
+//
+
+#ifndef SETTINGSMENU_H
+#define SETTINGSMENU_H
+
+
+
+class SettingsMenu {
+
+};
+
+
+
+#endif //SETTINGSMENU_H
